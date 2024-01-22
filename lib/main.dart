6ca7bc52cda8a81b20123a31cca85b1b28d01ec3
ABOKAJR <marie.edmond.dossa@gmail.com>
2024-01-22 19:11:54 +0100

    mise en place de la fonction de capture de photo etd'extraction de texte

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:extract_text_picture/views/home.dart';
+import 'package:extract_text_picture/views/navBar.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MyApp());
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+  runApp(MyApp(cameras: cameras));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required this.cameras});
+  final List<CameraDescription> cameras;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NavBar(cameras: cameras),
+    );
+  }
 }
